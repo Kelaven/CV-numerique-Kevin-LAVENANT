abeffffbf30d242ea9f23da74ae9a097725dea2d
Kevin Lavenant <kevinlavenant@MBP-de-Kevin.lan>
2023-10-11 00:38:05 +0200

    test apparition image pour les diplomes

---
+++ public/assets/js/script.js
@@ -0,0 +1,8 @@
+// const displayAnimation = document.getElementById("testjs")
+// // console.log(displayAnimation);
+
+
+
+// displayAnimation.addEventListener("mouseover",  () => {
+//     displayAnimation.classList.add('animate__animated', 'animate__fadeInUp');
+// })
